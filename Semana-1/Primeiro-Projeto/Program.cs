5ebddf41285aed426965a990dbae062d51a3e04e
AlanPrates <alan_nz@live.com>
2023-11-07 17:35:49 -0300

    first commit

--- Semana-1/Primeiro-Projeto/Program.cs
+++ Semana-1/Primeiro-Projeto/Program.cs
@@ -4,8 +4,9 @@
         double tipoDouble;
         decimal tipDecimal;
         long tipoLong;
-        
+
         tipoInteiro = int.MaxValue;
+
 
         Console.WriteLine($"Este valor {tipoInteiro} eh o maior inteiro possivel");
 #endregion        
