4ddf852c0e6ed8461d0351afaafa074352e9734d
AlanPrates <alan_nz@live.com>
2023-11-08 09:48:29 -0300

    projeto finalizado

--- dotNET-P001/Program.cs
+++ dotNET-P001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// variável do tipo double em um tipo int, você pode usar a conversão explícita. 
+// A parte fracionária será truncada, ou seja, descartada. Se a parte fracionária 
+// não puder ser representada em um int, ela será arredondada para o número inteiro 
+// mais próximo (para baixo em direção a zero).
+
+double numeroDouble = 10.8;
+int numeroInteiro = (int)numeroDouble;
+
+Console.WriteLine(numeroInteiro); // O resultado será 10
+
