a2ead6a21aa78d4904edffeda7f9094effa6598d
AlanPrates <alan_nz@live.com>
2023-11-14 15:19:19 -0300

    First commit

---
+++ Semana-2/Program.cs
@@ -0,0 +1,31 @@
+﻿// region  Foreach examples
+// string[] people = {"Ana", "Alan", "Alana","Maria", "João", "José", "Pedro"};
+
+// foreach (string person in people)
+//{
+//  Console.WriteLine(person);
+//}
+
+// Exercicio 1 
+
+// for (int i = 0; i <= 30; i++)
+// {
+// if (i % 3 == 0 || i % 4 == 0)
+//{
+//  Console.WriteLine(i);
+//}
+//}
+
+// Exercicio 2 region Fibonacci
+
+
+//int a = 0, b = 1, c = 0;
+
+//while (c <= 100)
+//{
+//  Console.WriteLine(c);
+//  a = b;
+//  b = c;
+//   c = a + b;
+//}
+
