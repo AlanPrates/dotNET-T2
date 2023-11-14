5085c1c0b068208c15a776d9cda94d0c1f598436
AlanPrates <alan_nz@live.com>
2023-11-14 15:51:04 -0300

    Region String examples

--- Semana-2/Program.cs
+++ Semana-2/Program.cs
@@ -1,31 +1,45 @@
-﻿// region  Foreach examples
-// string[] people = {"Ana", "Alan", "Alana","Maria", "João", "José", "Pedro"};
+﻿//region  Foreach examples
+string[] people = { "Ana", "Alan", "Alana", "Maria", "João", "José", "Pedro" };
 
-// foreach (string person in people)
-//{
-//  Console.WriteLine(person);
-//}
+foreach (string person in people)
+{
+    Console.WriteLine(person);
+}
 
-// Exercicio 1 
+//Exercicio 1 
 
-// for (int i = 0; i <= 30; i++)
-// {
-// if (i % 3 == 0 || i % 4 == 0)
-//{
-//  Console.WriteLine(i);
-//}
-//}
+for (int i = 0; i <= 30; i++)
+{
+    if (i % 3 == 0 || i % 4 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
 
-// Exercicio 2 region Fibonacci
+//Exercicio 2 region Fibonacci
 
 
-//int a = 0, b = 1, c = 0;
+int a = 0, b = 1, c = 0;
 
-//while (c <= 100)
-//{
-//  Console.WriteLine(c);
-//  a = b;
-//  b = c;
-//   c = a + b;
-//}
+while (c <= 100)
+{
+    Console.WriteLine(c);
+    a = b;
+    b = c;
+    c = a + b;
+}
 
+
+//region String examples
+
+Console.WriteLine("What's your name?");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name}");
+
+string[] names = name.Split(' ');
+foreach (var item in names)
+{
+    Console.WriteLine(item);
+}
+
+#endregion
