f5f013b1effa9590b561cacfb90abe3d81ddb579
AlanPrates <alan_nz@live.com>
2023-11-14 23:51:39 -0300

    Task.cs

--- dotNET-P002/Program.cs
+++ dotNET-P002/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task.cs
+public class Task
+{
+    public string Title { get; }
+    public string Description { get; }
+    public DateTime DueDate { get; }
+    public bool IsCompleted { get; private set; }
+
+    public Task(string title, string description, DateTime dueDate)
+    {
+        Title = title;
+        Description = description;
+        DueDate = dueDate;
+        IsCompleted = false;
+    }
+
+    public void MarkAsCompleted()
+    {
+        IsCompleted = true;
+    }
+
+    public override string ToString()
+    {
+        return $"Tarefa: {Title}\nDescrição: {Description}\nData de Vencimento: {DueDate}\nStatus: {(IsCompleted ? "Concluída" : "Pendente")}\n";
+    }
+}
