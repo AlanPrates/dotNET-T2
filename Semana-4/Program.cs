363228ad4293e5f354f25cef2e0022fe8f774557
alanprates <alan_nz@live.com>
2023-11-28 14:59:25 -0300

    ContaBancaria

--- Semana-4/Program.cs
+++ Semana-4/Program.cs
@@ -1,1 +1,48 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+class ContaBancaria
+{
+    private decimal saldo;
+
+    // Propriedade Saldo com lógica personalizada para evitar saldos negativos
+    public decimal Saldo
+    {
+        get { return saldo; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Saldo não pode ser negativo");
+            }
+            saldo = value;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ContaBancaria minhaConta = new ContaBancaria();
+
+        try
+        {
+            // Solicita ao usuário para inserir o saldo desejado
+            Console.Write("Digite o saldo inicial da conta: ");
+            decimal saldoInicial = decimal.Parse(Console.ReadLine());
+
+            // Tenta definir o saldo inserido
+            minhaConta.Saldo = saldoInicial;
+
+            // Exibe o saldo atual
+            Console.WriteLine($"Saldo atual: {minhaConta.Saldo}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Valor inválido inserido. Certifique-se de inserir um número válido.");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
