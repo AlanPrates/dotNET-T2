f46272260cf33b219dd655900111c65014afe8a2
alanprates <alan_nz@live.com>
2023-11-28 15:03:06 -0300

    Veículo

--- Semana-4/Program.cs
+++ Semana-4/Program.cs
@@ -46,3 +46,46 @@ class Program
         }
     }
 }
+
+class Veiculo
+{
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public string Cor { get; set; }
+
+    // Propriedade somente leitura para calcular a idade do veículo
+    public int IdadeVeiculo
+    {
+        get
+        {
+            int anoAtual = DateTime.Now.Year;
+            return anoAtual - Ano;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Cria uma instância da classe Veiculo
+        Veiculo meuVeiculo = new Veiculo();
+
+        // Interage com o usuário para obter informações do veículo
+        Console.Write("Digite o modelo do veículo: ");
+        meuVeiculo.Modelo = Console.ReadLine();
+
+        Console.Write("Digite o ano do veículo: ");
+        meuVeiculo.Ano = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a cor do veículo: ");
+        meuVeiculo.Cor = Console.ReadLine();
+
+        // Exibe as informações do veículo no console
+        Console.WriteLine("\nInformações do Veículo:");
+        Console.WriteLine($"Modelo: {meuVeiculo.Modelo}");
+        Console.WriteLine($"Ano: {meuVeiculo.Ano}");
+        Console.WriteLine($"Cor: {meuVeiculo.Cor}");
+        Console.WriteLine($"Idade do Veículo: {meuVeiculo.IdadeVeiculo} anos");
+    }
+}
