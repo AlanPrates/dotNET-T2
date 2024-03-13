fed839f3c78e212f7386873d7231076d5aa2f02a
AlanPrates <alan_nz@live.com>
2024-03-13 09:51:43 -0300

    Exercício 1

---
+++ Lampada/Lampada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando uma lâmpada inicialmente ligada
+        Lampada lampada = new Lampada(true);
+        
+        // Imprimindo o estado atual
+        Console.WriteLine("Estado inicial:");
+        lampada.Imprimir();
+        
+        // Desligando a lâmpada e imprimindo o estado atual
+        lampada.Desligar();
+        lampada.Imprimir();
+        
+        // Ligando a lâmpada e imprimindo o estado atual
+        lampada.Ligar();
+        lampada.Imprimir();
+    }
+}
